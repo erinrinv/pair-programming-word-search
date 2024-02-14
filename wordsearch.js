9b289e43a57421777edf7857fc32b1e1b255dfdd
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-14 14:56:09 -0700

    fixed the buggy code and added what was need by the assignment and review it over with partner in the group

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
+
+    // Creates new array iterates over sub array and combines into single
+    // string 
+    // converts each row of the grid into a string
+    // horizontal letters
     const horizontalJoin = letters.map(ls => ls.join(''))
+
+    //Creates new array and iterates over array and combines into string
+    // vertical letters
+    const verticalJoin = letters[0].map((col, index) => letters.map(row => row[index]));
+    const verticalJoinFlipped = verticalJoin.map(col => col.join(""));
+
+    // Chckes to see if word is in horizontal list
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    
+    for (let line of verticalJoinFlipped) {
+        if (line.includes(word)) return true;
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
